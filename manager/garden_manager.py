7f837642e4a9b10d979bda262c858d27851da2e7
OleksiuDatsko <datskooleksiu@gmail.com>
2023-05-19 14:02:07 +0300

    added new classes, changed logic of class methods

---
+++ manager/garden_manager.py
@@ -0,0 +1,68 @@
+"""import abstract class"""
+from  ..models.garden import Garden
+
+
+class GardenManager:
+    """
+    Class representing a Garden Manager.
+
+    Attributes:
+        gardens (list): A list of Garden objects.
+
+    Methods:
+        add_garden(garden)
+            Adds a Garden object to the manager.
+        add_gardens(gardens)
+            Adds multiple Garden objects to the manager.
+        find_all_with_vegetable_garden()
+            Finds all gardens with a vegetable garden.
+        find_all_with_orchard()
+            Finds all gardens with an orchard.
+    """
+
+    def __init__(self, gardens: Garden = None) -> None:
+        self.gardens = list(gardens) if gardens else []
+
+    def add_garden(self, garden: Garden) -> None:
+        """
+        Adds a Garden object to the manager.
+
+        Args:
+            garden (Garden): The Garden object to add.
+        """
+        self.gardens.append(garden)
+
+    def add_gardens(self, gardens) -> None:
+        """
+        Adds multiple Garden objects to the manager.
+
+        Args:
+            gardens (list): A list of Garden objects.
+        """
+        for garden in gardens:
+            self.gardens.append(garden)
+
+    def find_all_with_vegetable_garden(self) -> list:
+        """
+        Finds all gardens with a vegetable garden.
+
+        Returns:
+            list: A list of Garden objects with a vegetable garden.
+        """
+        return [garden for garden in self.gardens if garden.has_vegetable_garden()]
+        # return list(filter(lambda garden: garden.has_vegetable_garden(), self.gardens))
+
+    def find_all_with_orchard(self) -> list:
+        """
+        Finds all gardens with an orchard.
+
+        Returns:
+            list: A list of Garden objects with an orchard.
+        """
+        # return [garden for garden in self.gardens if garden.has_orchard()]
+        return list(filter(lambda garden: garden.has_orchard(), self.gardens))
+
+    def __str__(self):
+        result = "Garden manager:\n"
+        result += "\n".join([str(garden) for garden in self.gardens])
+        return result
---
+++ models/garden.py
@@ -0,0 +1,55 @@
+""""""
+from abc import ABC, abstractmethod
+
+
+class Garden(ABC):
+    """
+    An abstract class representing a garden.
+
+    Attributes:
+        area (float): The area of the garden.
+
+    Methods:
+        plant_flower(self, count)
+            Adds a specified number of flowers to the garden.
+        remove_flower(self, count)
+            Removes a specified number of flowers from the garden.
+        add_vegetable_region(self, area)
+            Adds an area to the garden.
+
+    Properties:
+        area
+            The area of the garden in hectars.
+    """
+
+    @abstractmethod
+    def __init__(
+        self,
+        area: float = None,
+    ) -> None:
+        self.area = area
+
+    @abstractmethod
+    def has_vegetable_garden(self) -> bool:
+        """
+        Check is garden have vegetable garden
+
+        Returns:
+            bool: if garden has vegeable garden
+        """
+
+    @abstractmethod
+    def has_orchard(self) -> bool:
+        """
+        Check if garden has orchard
+
+        Returns:
+            bool: if garden has orchard
+        """
+
+    @abstractmethod
+    def __str__(self, **kwargs) -> str:
+        result = f"{kwargs.pop('class_name', 'Garden')}: "
+        for attributes, values in kwargs.items():
+            result += f"{attributes} = {values}; "
+        return result
